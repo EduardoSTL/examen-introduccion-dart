1dc03872fcde19d8f643a7cc4a4f48c5e9202ba9
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-08 10:29:53 -0600

    Initial commit

---
+++ bin/examen_introduccion_dart.dart
@@ -0,0 +1,5 @@
+import 'package:examen_introduccion_dart/examen_introduccion_dart.dart' as examen_introduccion_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${examen_introduccion_dart.calculate()}!');
+}
---
+++ test/examen_introduccion_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:examen_introduccion_dart/examen_introduccion_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
