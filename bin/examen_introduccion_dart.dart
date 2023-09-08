25204a1580c645b042b1e144d247cc782a389e06
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-08 12:06:27 -0600

    updt: 2

--- bin/examen_introduccion_dart.dart
+++ bin/examen_introduccion_dart.dart
@@ -6,8 +6,9 @@ abstract class Departamento{
 mixin Dept on Departamento{
   // ignore: prefer_typing_uninitialized_variables
   var departamento;
+  List cantidadArticulos = ['1', '2', '3'];
   void printDepartamento() {
-    print(departamento);
+    print('Departamento: $departamento, Cantidad de articulos: $cantidadArticulos');
   }
 }
 
@@ -16,29 +17,46 @@ enum Categoria{celulares, limpieza, hombre}
 class Articulos extends Departamento with Dept{
   String nombre; 
   final Categoria categoria;
+  final int cantidad;
 
   @override
   var departamento;
+  @override
+  List cantidadArticulos =['1','2', '3'];
 
   Articulos({
     required this.nombre,
     required this.categoria,
+    required this.cantidad,
+    required this.cantidadArticulos,
     required this.departamento
-  })
+  });
 
   @override
   String toString(){
     final Categoria = categoria.toString().split('.').last;
-    return 'Nombre: $nombre, Categoria: $Categoria';
+    return 'Nombre: $nombre, Cantidad: $cantidad, Categoria: $Categoria,  Departamento: $departamento';
   }
 
   factory Articulos.create({
     required String nombre,
     required Categoria categoria,
+    required int cantidad,
+    required List cantidadArticulos,
+    required String departamento,
   }) {
     return Articulos(
       nombre: nombre,
       categoria: categoria,
+      cantidad: cantidad,
+      cantidadArticulos: cantidadArticulos,
+      departamento: departamento,
     );
   }
+}
+
+void main() {
+  Articulos articulos1 = Articulos.create(nombre: 'Monitor ACER', categoria: Categoria.celulares, cantidad: 8, cantidadArticulos: List, departamento: 'Tecnologia');
+  articulos1.printDepartamento(); 
+  print(articulos1); 
 }
